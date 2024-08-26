d8ecaf4756f9bb702bba1cc6e5b3681059d5fffd
ryz <knyazevskaya.a.a@gmail.com>
2024-08-26 23:59:16 +0300

    v 1.0 (one button)

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,73 +1,50 @@
-import React, {useState} from 'react';
-import axios from 'axios';
-import autosize from "autosize/dist/autosize";
-import $ from 'jquery';
 
-/*
-https://doka.guide/css/grid-guide/
-https://doka.guide/css/flexbox-guide/
-https://qna.habr.com/q/1131854
-https://habr.com/ru/companies/ruvds/articles/445276/
-https://legacy.reactjs.org/docs/hooks-effect.html
-https://react.dev/learn/you-might-not-need-an-effect
-https://react.dev/learn/describing-the-ui
-https://music.yandex.ru/home
-https://calendar.yandex.ru/week?uid=862755172
-https://www.kinopoisk.ru/?utm_referrer=www.yandex.ru
-https://gitlab.figvam.ru/users/sign_in
-https://ru.stackoverflow.com/
-https://css-tricks.com/
-https://doka.guide/js/promise/
-https://stepik.org/catalog/04
-https://www.simplilearn.com/tutorials/docker-tutorial/what-is-dockerfile
-https://habr.com/ru/companies/docsvision/articles/335988/
-*/
 
 function Main() {
 
-    const [state, setState] = useState({});
-
-    let getResult = () => {
-        getLinesArray();
-        getTable();
+    let data = {
+        urls: ['']
     }
 
-    let getLinesArray = () => {
+    function createLinesArray(btnCount) {
         let textaeraVal = document.querySelector('#textarea').value;
         let linesArray = textaeraVal.split(/\n/);
-        console.log(linesArray);
-        getStatus(linesArray)
+        Object.assign(data.urls, linesArray);
     }
 
-    let getStatus = (linesArray) => {
-        return axios({
+    async function response(linesArray, res) {
+        await fetch("http://localhost:8000/api/v1/search", {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            method: 'POST',
-            url: 'http://localhost:8000/api/v1/search',
-            data: {
-                "urls": linesArray,
-            }
+            body: JSON.stringify(data)
         })
-            .then(response => setState(JSON.stringify(response.data)))
+            .then(response => response.json())
+            .then(result => createTable(result))
     }
 
-    let getTable = () => {
-        const stateData = state.toString();
-        const data = JSON.parse(stateData);
-        const table = document.createElement('table');
+    function createTable(result) {
+        const table = document.getElementById('table');
+        table.setAttribute('border', '1');
+        const headerRow = document.createElement('tr');
+        Object.keys(result[0]).forEach(key => {
+            const th = document.createElement('th');
+            th.appendChild(document.createTextNode(key));
+            headerRow.appendChild(th);
+        });
+        table.appendChild(headerRow);
 
-        data.forEach((item) => {
-            const tableRow = table.insertRow();
-            Object.values(item).forEach((text) => {
-                const tableCell = tableRow.insertCell();
-                tableCell.textContent = text;
-            })
-        })
-        table.setAttribute("class", "table");
+        result.forEach(item => {
+            const row = document.createElement('tr');
+            Object.values(item).forEach(value => {
+                const td = document.createElement('td');
+                td.appendChild(document.createTextNode(value));
+                row.appendChild(td);
+            });
+            table.appendChild(row);
+        });
         document.body.appendChild(table);
-        autosize($('textarea'));
         highlight();
     }
 
@@ -75,6 +52,7 @@ function Main() {
         let patterns = [
             {text: "200 OK", classes: ['green']},
             {text: "500 Internal Server Error", classes: ['red']},
+            {text: "403 Forbidden", classes: ['yellow']},
         ];
         const marker = ( text, patterns ) => {
             return patterns.reduce( ( result, current ) => {
@@ -90,14 +68,16 @@ function Main() {
         document.body.innerHTML = marker( text, patterns );
     }
 
-    return (
+    return(
         <div>
-            <div className="divBtn">
-                <button id='btn' onClick={getLinesArray}>Create request</button>
-                <button id='btn-2' onClick={getTable}>Get table with result</button>
+            <div>
+                <textarea id="textarea" onChange={createLinesArray}/>
             </div>
-            <div contentEditable className="divText">
-                <textarea name="textarea" id="textarea" cols="30" rows="10"></textarea>
+            <div>
+                <button onClick={response}>Click me</button>
+            </div>
+            <div>
+                <table id='table'></table>
             </div>
         </div>
     )
