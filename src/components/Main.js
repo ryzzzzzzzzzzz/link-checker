956542ce085714c6f33e69e62edb4d921d8d803a
ryz <knyazevskaya.a.a@gmail.com>
2024-08-30 23:29:27 +0300

    v_2.0

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,71 +1,62 @@
-
+import React, {useState} from "react";
+import "./../App.css"
 
 function Main() {
+
+    const [counter200, setCounter200] = useState(0);
+    const [counter403, setCounter403] = useState(0);
+    const [counter500, setCounter500] = useState(0);
+    const [counterOther, setCounterOther] = useState(0);
+    const [loading, setLoading] = useState(false);
 
     let data = {
         urls: ['']
     }
 
-    function createLinesArray(btnCount) {
+    function createLinesArray() {
         let textaeraVal = document.querySelector('#textarea').value;
         let linesArray = textaeraVal.split(/\n/);
         Object.assign(data.urls, linesArray);
     }
 
     async function response(linesArray, res) {
-        await fetch("http://localhost:8000/api/v1/search", {
+        setLoading(true);
+        document.getElementById('textarea').value = '';
+        await fetch(process.env.REACT_APP_FETCH_URL, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
         })
-            .then(response => response.json())
-            .then(result => createTable(result))
-    }
+            .then(result => result.json())
+            .then(result => fillTable(result))}
 
-    function createTable(result) {
-        const table = document.getElementById('table');
-        table.setAttribute('border', '1');
-        const headerRow = document.createElement('tr');
-        Object.keys(result[0]).forEach(key => {
-            const th = document.createElement('th');
-            th.appendChild(document.createTextNode(key));
-            headerRow.appendChild(th);
-        });
-        table.appendChild(headerRow);
+    function fillTable(result) {
+        let tbodyRef = document.getElementById('table').getElementsByTagName('tbody')[0];
 
-        result.forEach(item => {
-            const row = document.createElement('tr');
-            Object.values(item).forEach(value => {
-                const td = document.createElement('td');
-                td.appendChild(document.createTextNode(value));
-                row.appendChild(td);
-            });
-            table.appendChild(row);
-        });
-        document.body.appendChild(table);
-        highlight();
-    }
-
-    let highlight = () => {
-        let patterns = [
-            {text: "200 OK", classes: ['green']},
-            {text: "500 Internal Server Error", classes: ['red']},
-            {text: "403 Forbidden", classes: ['yellow']},
-        ];
-        const marker = ( text, patterns ) => {
-            return patterns.reduce( ( result, current ) => {
-                let regexp = new RegExp(current.text, 'g');
-                let style = current.classes.join(' ');
-
-                return result.replace( regexp, `<span class="${ style }">${ current.text }</span>` );
-            }, text );
-        }
-
-        let text = document.body.innerHTML;
-
-        document.body.innerHTML = marker( text, patterns );
+        result.forEach(row => {
+            let newRow = tbodyRef.insertRow();
+            Object.values(row).forEach(value => {
+                let newCell = newRow.insertCell();
+                let newText = document.createTextNode(value);
+                newCell.appendChild(newText);
+                switch (value) {
+                    case '200 OK': newCell.innerHTML = '<span class="green">' + value + '</span>';
+                    setCounter200(counter200 + 1);
+                    break;
+                    case '403 Forbidden':  newCell.innerHTML = '<span class="yellow">' + value + '</span>';
+                    setCounter403(counter403 + 1);
+                    break;
+                    case '500 Internal Server Error':  newCell.innerHTML = '<span class="red">' + value + '</span>';
+                    setCounter500(counter500 + 1);
+                    break;
+                    default: setCounterOther(counterOther + 1);
+                    break;
+                }
+            })
+        })
+        setLoading(false);
     }
 
     return(
@@ -74,10 +65,29 @@ function Main() {
                 <textarea id="textarea" onChange={createLinesArray}/>
             </div>
             <div>
-                <button onClick={response}>Click me</button>
+                <button onClick={response}>Check links</button>
             </div>
             <div>
-                <table id='table'></table>
+                <div>Code 200: {counter200}</div>
+                <div>Code 403: {counter403}</div>
+                <div>Code 500: {counter500}</div>
+                <div>Other: {counterOther}</div>
+            </div>
+            <div>
+                {loading === true && 'Loading...'}
+            </div>
+            <div>
+                <table id='table'>
+                    <thead>
+                    <tr>
+                        <th>URL</th>
+                        <th>CODE</th>
+                        <th>ERROR</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
             </div>
         </div>
     )
